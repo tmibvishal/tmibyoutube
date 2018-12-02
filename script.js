f9a5c425ac3452949e4fe222a12600764c501734
Vishal Singh <tmibvishal@gmail.com>
2018-12-02 18:01:10 +0530

    fix download button

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function sendvidUrl(){
-    let wholeUrl = document.getElementById("yurl").value;
-    //https://www.youtube.com/watch?v=IWJUphbYnpg
-    let first = wholeUrl.indexOf("?")+1;
-    let end = wholeUrl.length;
-    wholeUrl = wholeUrl.slice(first, end);
-    let vidId = (wholeUrl.split("=")[1]);
-    //console.log(vidId);
+console.log("fdfd");
 
+window.onload = function(){
+    document.getElementById("getVidBut").addEventListener("click", sendvidUrl);
+}
+
+function sendvidUrl(){
     document.getElementById("form1").submit();
 }
